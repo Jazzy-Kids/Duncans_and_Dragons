7503bf976a712a648055321aa8b8a514cb63942a
Benjamin <benjazzy64@gmail.com>
2017-05-14 18:41:55 -0400

    Added Camera Control Script
    The script takes in user input and moves the player object.

---
+++ Assets/Scrips/CameraControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public float speed = 10.0f;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal") * speed;
+        float vertical = Input.GetAxis("Vertical") * speed;
+
+        horizontal *= Time.deltaTime;
+        vertical *= Time.deltaTime;
+
+        transform.Translate(horizontal, vertical, 0);
+	}
+}
