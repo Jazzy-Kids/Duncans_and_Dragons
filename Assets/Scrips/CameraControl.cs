4a3905d0852e1bf9d6b74bcd65b84c3b12070602
Benjamin <benjazzy64@gmail.com>
2017-05-15 17:42:35 -0400

    Made multiplayer work with test scene
    Changed Camera Control script so that only your camera would be
    rendered.

--- Assets/Scrips/CameraControl.cs
+++ Assets/Scrips/CameraControl.cs
@@ -1,19 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class CameraControl : MonoBehaviour {
+public class CameraControl : NetworkBehaviour {
 
     public float speed = 10.0f;
     public float sprintMultipier = 2.0f;    //A value of 1 means sprint speed is double normal speed
+    public Camera cam;
+
 
 	// Use this for initialization
 	void Start () {
-		
+        Debug.Log(isLocalPlayer);
+
+        if (!isLocalPlayer)
+            cam.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (!isLocalPlayer)
+            return;
+
+
         float sprint = sprintMultipier * Input.GetAxis("Sprint") + 1;
         
         float horizontal = Input.GetAxis("Horizontal") * speed * sprint;
